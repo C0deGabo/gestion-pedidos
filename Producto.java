2bee8bd8292d6c29c7cbc1512caf966329c2eda2
Gabriel Rivera Ferreyra <rive.gbo@gmail.com>
2026-09-09 22:08:34 -0500

    Gabriel Rivera-- estructura inicial del proyecto Java

---
+++ Producto.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("--- Sistema de Gestión de Pedidos ---");
+        Producto producto1 = new Producto("Café Americano", 3.50);
+        producto1.mostrarDatos();
+    }
+}
